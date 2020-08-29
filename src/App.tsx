a7d9edb14e0b73c49e7a30184f570bdf0d0fe86a
Hanwen Cheng <heawen.cheng@gmail.com>
2020-08-29 21:35:34 +0200

    move api load at start

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import NavigationBar from 'react-native-navbar-color';
 
 import { AppNavigator } from './screens';
 
+import { useApi } from 'modules/token/hooks';
+import ApiNotLoaded from 'modules/main/components/ApiNotLoaded';
 import { useScannerContext, ScannerContext } from 'stores/ScannerContext';
 import { useAccountContext, AccountsContext } from 'stores/AccountsContext';
 import CustomAlert from 'components/CustomAlert';
@@ -32,12 +34,13 @@ export default function App(): React.ReactElement {
 			'Non-serializable values were found in the navigation state' // https://reactnavigation.org/docs/troubleshooting/#i-get-the-warning-non-serializable-values-were-found-in-the-navigation-state
 		]);
 	}
-
+	const isApiReady = useApi();
 	const alertContext = useAlertContext();
 	const seedRefContext = useSeedRefStore();
 	const accountsContext = useAccountContext();
 	const scannerContext = useScannerContext();
 
+	if (!isApiReady) return <ApiNotLoaded />;
 	return (
 		<SafeAreaProvider>
 			<AccountsContext.Provider value={accountsContext}>
--- src/modules/main/screens/Main.tsx
+++ src/modules/main/screens/Main.tsx
@@ -2,7 +2,6 @@ import React, { useContext } from 'react';
 
 import PathsList from 'modules/main/components/PathsList';
 import ApiNotLoaded from 'modules/main/components/ApiNotLoaded';
-import { useApi } from 'modules/token/hooks';
 import NoCurrentIdentity from 'modules/main/components/NoCurrentIdentity';
 import { SafeAreaViewContainer } from 'components/SafeAreaContainer';
 import OnBoardingView from 'modules/main/components/OnBoading';
@@ -11,12 +10,10 @@ import { AccountsStoreStateWithIdentity } from 'types/identityTypes';
 
 export default function Main(): React.ReactElement {
 	const accountsStore = useContext(AccountsContext);
-	const isApiReady = useApi();
 	const { identities, currentIdentity, loaded, accounts } = accountsStore.state;
 	const hasLegacyAccount = accounts.size !== 0;
 
 	if (!loaded) return <SafeAreaViewContainer />;
-	if (!isApiReady) return <ApiNotLoaded />;
 	if (identities.length === 0)
 		return <OnBoardingView hasLegacyAccount={hasLegacyAccount} />;
 	if (currentIdentity === null) return <NoCurrentIdentity />;
